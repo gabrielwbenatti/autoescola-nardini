fa1310c285fbc96e5e083d9df8b1c7dcec31a5ce
Gabriel Wisky Benatti <gabrielwiskybenatti@gmail.com>
2022-07-05 21:40:37 -0300

    viewport

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,15 @@
+import React from "react";
+import { addDecorator } from "@storybook/react";
+
+import GlobalStyle from "../src/components/GlobalStyle";
+
+addDecorator((storyFn) => (
+  <>
+    <GlobalStyle />
+    {storyFn()}
+  </>
+));
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +18,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
